b3baec398c7893af34d9e92aebb92ad6299c364c
MajorDong <odsts@outlook.com>
2020-02-29 22:45:19 +0800

    基本完成todo组件

---
+++ src/components/task/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
+        task
+      </div>
+     );
+  }
+}
+ 
+export default Task;
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,28 +1,59 @@
 import React from 'react';
+import { useMemo } from 'react'
 import PropTypes from 'prop-types'
-
+import Task from '../task/Task'
+import { today, tomorrow } from '../../shared'
+import './Todo.scss'
 
 
 const Todo = (props) => {
   let { todo, selected } = props
 
-  const progress = () => {
-    let allCount = todo.tasks.filter( task => !task.deleted).length
-    let doneCount = todo.tasks.filter( task => !task.deleted && task.done).length
-    let numStr = Math.round(doneCount / allCount ) * 100 + '%'
+  const progress = useMemo(() => {
+    let allCount = todo.tasks.filter(task => !task.deleted).length
+    let doneCount = todo.tasks.filter(task => !task.deleted && task.done).length
+    let numStr = Math.round(doneCount / allCount) * 100 + '%'
     return numStr
-  }
+  },
+    [todo]
+  )
 
-  const progressColor = ()=> {
+  const progressColor = useMemo(() => {
     const colorLeft = `color-stop(30%, ${todo.colors[0]})`
     const colorRight = `to(${todo.colors[1]})`
     return `-webkit-gradient(linear, left bottom, right bottom, ${colorLeft}, ${colorRight})`
-  }
+  },
+    [todo]
+  )
 
-  return(
+  const todayTasks = useMemo(()=>{
+    const todoCount = todo.tasks.filter( task => 
+      task.date >= today && task.date < tomorrow)
+    return todoCount
+  },
+    [todo]
+  )
+
+  const tomorrowTasks = useMemo(()=>{
+    const tomorrowCount = todo.tasks.filter( task => 
+      task.date > tomorrow)
+    return tomorrowCount
+  },
+    [todo]
+  )
+
+  const outdateTasks = useMemo(()=>{
+    const outdateCount = todo.tasks.filter( task => 
+      task.date < today)
+    return outdateCount
+  },
+    [todo]
+  )
+
+  return (
     <div className={selected ? 'todo todo-selected' : 'todo'}>
       <div className="todo-head">
-        <div className="todo-icon" style={{color: todo.colors[0]}}>
+        <div className="todo-icon" style={{ color: todo.colors[0] }}>
           <i className={`fa fa-${todo.icon}`}></i>
         </div>
         <div className="todo-menu">
@@ -35,11 +66,49 @@ const Todo = (props) => {
         <div className="todo-progress">
           <span className="todo-progress-line">
             <i style={{
-              width: progress(),
-              backgroundImage : progressColor()
+              width: progress,
+              backgroundImage: progressColor
             }}></i>
           </span>
-          <span className="todo-progress-num">{progress()}</span>
+          <span className="todo-progress-num">{progress}</span>
+        </div>
+        <div className="todo-tasks">
+          { todayTasks.length > 0 && (
+            <div className="todo-tasks-today">
+              <h4>Today</h4>
+              <ul>
+                {todayTasks.map( task => (
+                  <li key={task.id}>
+                    <Task></Task>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          { tomorrowTasks.length > 0 && (
+            <div className="todo-tasks-today">
+              <h4>Tomorrow</h4>
+              <ul>
+                {tomorrowTasks.map( task => (
+                  <li key={task.id}>
+                    <Task></Task>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          { outdateTasks.length > 0 && (
+            <div className="todo-tasks-today">
+              <h4>Outdate</h4>
+              <ul>
+                {outdateTasks.map( task => (
+                  <li key={task.id}>
+                    <Task></Task>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
