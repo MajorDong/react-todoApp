818f61430cc816b992a670726855810824695af1
MajorDong <odsts@outlook.com>
2020-03-09 10:15:12 +0800

    Updates

---
+++ precache-manifest.63834e937e8d61e06e127a9123b02fcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0faf38be394835b2c65397fb88c43e",
+    "url": "/react-todoApp/index.html"
+  },
+  {
+    "revision": "03eb92f356662ce3143a",
+    "url": "/react-todoApp/static/css/2.8e3fbfdf.chunk.css"
+  },
+  {
+    "revision": "0a10084316d291e782b0",
+    "url": "/react-todoApp/static/css/main.f55cd0e3.chunk.css"
+  },
+  {
+    "revision": "03eb92f356662ce3143a",
+    "url": "/react-todoApp/static/js/2.a38e7b90.chunk.js"
+  },
+  {
+    "revision": "e6ff647c2a6af9cdfc810fe3a8fc8680",
+    "url": "/react-todoApp/static/js/2.a38e7b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a10084316d291e782b0",
+    "url": "/react-todoApp/static/js/main.8d6fc857.chunk.js"
+  },
+  {
+    "revision": "ba393f5762b430c96eda",
+    "url": "/react-todoApp/static/js/runtime-main.c7fa4a27.js"
+  },
+  {
+    "revision": "93e1b2feef3edfc40a71947c9916c60d",
+    "url": "/react-todoApp/static/media/face.93e1b2fe.jpeg"
+  }
+]);
